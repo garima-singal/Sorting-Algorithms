dc299d953bb99401bd8a7e5ecf7e033999edfa35
Garima Singal <garimasingal13@gmail.com>
2025-11-17 14:39:50 +0530

    Commit

---
+++ BucketSort/BucketSort.cpp
@@ -0,0 +1,35 @@
+void insertionSort(vector<float>& bucket) {
+    for (int i = 1; i < bucket.size(); ++i) {
+        float key = bucket[i];
+        int j = i - 1;
+        while (j >= 0 && bucket[j] > key) {
+            bucket[j + 1] = bucket[j];
+            j--;
+        }
+        bucket[j + 1] = key;
+    }
+}
+
+
+void bucketSort(float arr[], int n) {
+  
+    vector<float> b[n];
+
+   
+    for (int i = 0; i < n; i++) {
+        int bi = n * arr[i];
+        b[bi].push_back(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        insertionSort(b[i]);
+    }
+
+
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < b[i].size(); j++) {
+            arr[index++] = b[i][j];
+        }
+    }
+}
